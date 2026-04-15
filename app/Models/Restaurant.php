3167d7963cb230ea3cb7856120a3c9e3e16ee50d
DedyRisol3 <siahaandedypranata@gmail.com>
2026-04-15 21:59:11 +0700

    Updated Restaurant model to add relationships: belongsTo User and hasMany MysteryBoxes

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    //
+    // Other model properties and methods
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function mysteryBoxes() {
+        return $this->hasMany(MysteryBox::class);
+    }
 }
