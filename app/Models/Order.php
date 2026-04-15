f1771570caf9209ce4a7e7796e95e8ae0da6b19c
DedyRisol3 <siahaandedypranata@gmail.com>
2026-04-15 21:59:23 +0700

    Update relationships in Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    // Existing code...
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function mysteryBox()
+    {
+        return $this->belongsTo(MysteryBox::class);
+    }
+
+    public function review()
+    {
+        return $this->hasOne(Review::class);
+    }
 }
